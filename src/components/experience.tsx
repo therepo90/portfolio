f0b8c4bd45f0af949293702c1155a047e59677a2
Tomasz Sz <t.szepczynski@gmail.com>
2018-07-15 23:09:34 +0200

    experience

---
+++ src/components/experience.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Component } from "react";
+
+export default class Experience extends Component {
+    render() {
+        return <div className="section experience">
+            <div className="section-header">
+                My experience
+                </div>
+            <div className="section-content">
+                Fancy stuff
+            </div>
+        </div>
+    }
+};
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,40 +1,12 @@
 import * as React from "react";
 import { Component } from "react";
+import AboutMe from "./about-me";
+import Experience from "./experience";
 
 export default class Main extends Component {
     render() {
         return <div className="page-container">
-            <div className="section about">
-                <div className="section-header">
-                    About me
-                </div>
-                <div className="section-content">
-                    <div className="section-highlights dotted-container">
-                        <div><img src="build/assets/me.jpg" className="about-me-img" /></div>
-                        <span className="about-me-name">Tomasz Szepczyński<br />-Software Developer<br /> <span className="about-me-bio">Born on 1990 in Cracow, Poland</span></span>
-                    </div>
-                    <div className="section-description dotted-container">
-                        Hello there, <br />
-                        I'm a passionate software developer, who likes to mess around with various technologies:
-                    <ul>
-                            <li>
-                                Web development
-                            </li>
-                            <li>
-                                Game development
-                            </li>
-                            <li>
-                                Virtual Reality / Augmented Reality
-                            </li>
-                            <li>
-                                Hacking
-                            </li>
-                        </ul>
-                        I like bringing fancy ideas to life.<br /> <br />
-                        Currently living in Cracow, employeed as a web developer.<br />
-                    </div>
-                </div>
-            </div>
+            <Experience />
         </div>
     }
 };
