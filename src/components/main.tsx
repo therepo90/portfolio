0698c605e312eda4d430dde5393068e4070db670
Tomasz Sz <t.szepczynski@gmail.com>
2018-07-15 22:37:16 +0200

    styling aboutme

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -9,8 +9,29 @@ export default class Main extends Component {
                     About me
                 </div>
                 <div className="section-content">
-                    <div className="section-description">
-                        Some desc
+                    <div className="section-highlights dotted-container">
+                        <div><img src="build/assets/me.jpg" className="about-me-img" /></div>
+                        <span className="about-me-name">Tomasz Szepczyński<br />-Software Developer<br /> <span className="about-me-bio">Born on 1990 in Cracow, Poland</span></span>
+                    </div>
+                    <div className="section-description dotted-container">
+                        Hello there, <br />
+                        I'm a passionate software developer, who likes to mess around with various technologies:
+                    <ul>
+                            <li>
+                                Web development
+                            </li>
+                            <li>
+                                Game development
+                            </li>
+                            <li>
+                                Virtual Reality / Augmented Reality
+                            </li>
+                            <li>
+                                Hacking
+                            </li>
+                        </ul>
+                        I like bringing fancy ideas to life.<br /> <br />
+                        Currently living in Cracow, employeed as a web developer.<br />
                     </div>
                 </div>
             </div>
