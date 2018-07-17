ebf373b6afa4ebd0901f946c194ffdce0b7718a9
Tomasz Sz <t.szepczynski@gmail.com>
2018-07-18 01:28:01 +0200

    preload image

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -13,43 +13,64 @@ import JobsCont from "./jobs-cont/jobs-cont";
 import JobsCont2 from "./jobs-cont-2/jobs-cont-2";
 import Navbar from "./navbar/navbar";
 
+const images = ['build/assets/gate/gate10.jpg', 'build/assets/gate/knocker-left.png', 'build/assets/gate/eye-left-2.png'];
+
 export default class Main extends Component {
+    imgLoaded = 0;
+    passedWillMountPhase = false;
+    allLoaded = false;
+    onLoadImage = (src: string) => () => {
+        if(!this.passedWillMountPhase) {
+            return ;
+        }
+        this.imgLoaded++;
+        if(this.imgLoaded >= images.length) {
+            console.log('All loaded');
+            this.allLoaded = true;
+            setTimeout(() => this.onLoadedImages(),0);
+        }
+    }
+
     state = {
         gateVisible: false,  // enable me please...
         showPage: false,
     }
 
-    componentDidMount() {
-        setTimeout(() => {
-            console.log('Gate must fall.');
-            this.setState({
-                gateVisible: false,
-            })
-        }, 5000);
-        setTimeout(() => {
-            this.setState({
-                showPage: true,
-            })
-        }, 700);
+    componentWillMount() {
+        this.passedWillMountPhase = true;
     }
+
+    componentDidMount() {
+        
+    }
+
+    onLoadedImages = () => {
+        this.setState({
+            gateVisible: true,
+            showPage: true,
+        })
+    }
+
     render() {
-        return <React.Fragment>
-            {this.state.gateVisible && <Gate />}
-            {this.state.showPage && <React.Fragment>
-                <Navbar />
-                <div className="page-container">
-                    {<AboutMe />}
-                    {<EarlyYears />}
-                    {<EarlyYearsCont />}
-                    <MidYears />
-                    <CollegeYears />
-                    <CollegeYearsCont />
-                    <Jobs />
-                    <JobsCont />
-                    <JobsCont2 />
-                </div>
-            </React.Fragment>
-            }
+        return <React.Fragment>   
+                {!this.allLoaded && <div>Loading ...</div>}         
+                {!this.allLoaded && images.map((src, i) => <img key={i} src={src} onLoad={this.onLoadImage(src)} onError={this.onLoadImage(src)} style={{display: 'none'}} /> ) }
+                {this.state.gateVisible && <Gate />}
+                {this.state.showPage && <React.Fragment>
+                    <Navbar />
+                    <div className="page-container">
+                        {<AboutMe />}
+                        {<EarlyYears />}
+                        {<EarlyYearsCont />}
+                        <MidYears />
+                        <CollegeYears />
+                        <CollegeYearsCont />
+                        <Jobs />
+                        <JobsCont />
+                        <JobsCont2 />
+                    </div>
+                </React.Fragment>
+                }
         </React.Fragment>
     }
 };
