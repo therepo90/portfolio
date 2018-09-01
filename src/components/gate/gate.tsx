baf468319a1f9c5705002e7411a07f451e1f81c4
therepo90 <t.szepczynski@gmail.com>
2018-09-01 17:08:27 +0200

    gate bem

--- src/components/gate/gate.tsx
+++ src/components/gate/gate.tsx
@@ -6,15 +6,15 @@ export default class Gate extends Component {
 
     render() {
         return <div className="gate-container">
-            <div className="gate left">
-                <img src="/assets/gate/knocker-left.png" className="knocker" />
-                <img src="/assets/gate/eye-left-2.png" className="knocker eye" />
-                <div className="shadow" />
+            <div className="gate gate--left">
+                <img src="/assets/gate/knocker-left.png" className="gate__knocker" />
+                <img src="/assets/gate/eye-left-2.png" className="gate__knocker gate__knocker--eye" />
+                <div className="gate__shadow" />
             </div>
-            <div className="gate right">
-                <img src="/assets/gate/knocker-left.png" className="knocker" />
-                <img src="/assets/gate/eye-left-2.png" className="knocker eye" />
-                <div className="shadow" />
+            <div className="gate gate--right">
+                <img src="/assets/gate/knocker-left.png" className="gate__knocker" />
+                <img src="/assets/gate/eye-left-2.png" className="gate__knocker gate__knocker--eye" />
+                <div className="gate__shadow" />
             </div>
         </div>
     }
