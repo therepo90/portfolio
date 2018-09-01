ba932e36c5767c11e028d0283623f2c5e2219ee3
therepo90 <t.szepczynski@gmail.com>
2018-09-01 17:12:14 +0200

    navbar bem

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,11 +6,11 @@ export default class Navbar extends Component {
 
     render() {
         return <div className="navbar">
-            <div className="navbar-links">
-                <a href="https://www.youtube.com/channel/UCGaMIyiWInKQV6CeKPTePyA"><img src="/assets/icons/yt2.png" className="navbar-img filled" /></a>
-                <a href="https://github.com/therepo90"><img src="/assets/icons/github.png" className="navbar-img filled" /></a>
-                <a href="mailto: tomasz.szepczynski@gmail.com"><img src="/assets/icons/email.png" className="navbar-img filled"/></a>
-                <a href="https://www.linkedin.com/in/tomasz-sz1000000/"><img src="/assets/icons/linkedin.png" className="navbar-img filled" style={{padding: '0.25rem'}} /></a>
+            <div className="navbar__links">
+                <a href="https://www.youtube.com/channel/UCGaMIyiWInKQV6CeKPTePyA"><img src="/assets/icons/yt2.png" className="navbar__img navbar__img--filled" /></a>
+                <a href="https://github.com/therepo90"><img src="/assets/icons/github.png" className="navbar__img navbar__img--filled" /></a>
+                <a href="mailto: tomasz.szepczynski@gmail.com"><img src="/assets/icons/email.png" className="navbar__img navbar__img--filled"/></a>
+                <a href="https://www.linkedin.com/in/tomasz-sz1000000/"><img src="/assets/icons/linkedin.png" className="navbar__img navbar__img--filled" style={{padding: '0.25rem'}} /></a>
             </div>
         </div>
     }
